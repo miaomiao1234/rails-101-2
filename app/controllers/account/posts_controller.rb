2272b8566a4dc7a77e95741fcd4c2c6aa7b644c0
miaoliang <miaoliang01@gmail.com>
2017-01-27 19:04:04 +0800

    user simple_format helper to decorrate

--- app/controllers/account/posts_controller.rb
+++ app/controllers/account/posts_controller.rb
@@ -2,5 +2,9 @@ class Account::PostsController < ApplicationController
   before_action :authenticate_user!
   def index
     @posts = current_user.posts
-  end 
+  end
+
+  def delete
+  end
+  
 end
